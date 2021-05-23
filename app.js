b2948f4bbf2514ec2a818c508cfbe17644c7104c
joaopinacio <joaopedro.triun@gmail.com>
2021-05-23 16:56:16 -0300

    Minor fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 9090;
 const HOST = process.env.HOST || '0.0.0.0';
 
 const corsOpts = {
-	origin: 'https://gestao-de-pedidos.vercel.app/',
+	origin: 'https://gestao-de-pedidos.vercel.app',
 	methods: ['GET', 'POST'],
 	allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
 	credentials: true
