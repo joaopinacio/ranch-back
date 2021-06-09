e610e50e88a375c1629694180166828c217ccb01
joaopinacio <joaopedro.triun@gmail.com>
2021-06-08 22:13:05 -0300

    Minor fix

--- app/config/twilio.js
+++ app/config/twilio.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC277ede00288325cfa1af0edfd8ca5f40'; 
-const authToken = '197b26c260872cdb9e82296fd7917ca8'; 
+const accountSid = 'AC277ede00288325cfa1af0edfd8ca5f40';
+const authToken = '5165a0e6946d1361b8eaf95cd4d6d2d9';
 const client = require('twilio')(accountSid, authToken); 
 
 module.exports = client;
